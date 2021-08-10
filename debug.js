11c1d721e6984be440d3ba40804037f2822ff5f9
Tim Carry <tim@pixelastic.com>
2021-08-10 23:49:13 +0200

    add debug script

---
+++ debug.js
@@ -0,0 +1,14 @@
+const postHelper = require('reddinx/helpers/posts.js');
+const recordHelper = require('reddinx/helpers/records.js');
+const config = require('reddinx/config.js');
+
+(async () => {
+  await config.init({
+    cachePath: './tmp/cache',
+  });
+  const rawData = await postHelper.fromIds('terrainbuilding', ['ovu0ki']);
+  const rawPost = rawData[0];
+
+  const record = await recordHelper.fromPost(rawPost);
+  console.info(record);
+})();
