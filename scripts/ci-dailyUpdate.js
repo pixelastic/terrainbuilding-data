d7d5f42546f8011b07e7d802ad56a86481d47fc3
Tim Carry <tim@pixelastic.com>
2021-09-05 15:25:33 +0200

    dev: update on-circle

--- scripts/ci-dailyUpdate.js
+++ scripts/ci-dailyUpdate.js
@@ -1,35 +1,31 @@
 const onCircle = require('on-circle');
-const dayjs = require('golgoth/dayjs');
-const run = require('firost/run');
-const consoleInfo = require('firost/consoleInfo');
-const _ = require('golgoth/lodash');
+const { dayjs, _ } = require('golgoth');
+const { run, consoleInfo } = require('firost');
 
 (async () => {
-  await onCircle.run(
-    async ({ success, gitChangedFiles, gitCommitAll, gitPush }) => {
-      consoleInfo('Running incremental update');
-      await run('yarn run data:incremental');
+  await onCircle.run(async ({ success, repo }) => {
+    consoleInfo('Running incremental update');
+    await run('yarn run data:incremental');
 
-      const changedFiles = await gitChangedFiles();
-      if (_.isEmpty(changedFiles)) {
-        success('No files changed, we stop early');
-      }
-
-      // Commit changes
-      const currentDate = dayjs.utc().format('YYYY-MM-DD');
-      const commitMessage = `chore(ci): Daily update (${currentDate})`;
-      consoleInfo(`${changedFiles.length} files changed, commiting`);
-      await gitCommitAll(commitMessage);
-
-      // And push
-      consoleInfo('Pushing changes to the repo');
-      await gitPush();
-
-      // Re-index into Algolia
-      consoleInfo('Updating Algolia index');
-      await run('yarn run data:indexing');
-
-      success('Daily data updated');
+    const status = await repo.status();
+    if (_.isEmpty(status)) {
+      success('No files changed, we stop early');
     }
-  );
+
+    // Commit changes
+    const currentDate = dayjs.utc().format('YYYY-MM-DD');
+    const commitMessage = `chore(ci): Daily update (${currentDate})`;
+    consoleInfo(`${status.length} files changed, commiting`);
+    await repo.commitAll(commitMessage, { skipHooks: true, skipCi: true });
+
+    // And push
+    consoleInfo('Pushing changes to the repo');
+    await repo.push();
+
+    // Re-index into Algolia
+    consoleInfo('Updating Algolia index');
+    await run('yarn run data:indexing');
+
+    success('Daily data updated');
+  });
 })();
